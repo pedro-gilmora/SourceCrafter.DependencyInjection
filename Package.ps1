b485252be9dd350be46cbd6a1c5d23a3449bbd0b
pedro.gilmora <pedro.gilmora@outlook.es>
2026-07-24 13:59:29 -0400

    [Release] Fixing deployment script

--- Package.ps1
+++ Package.ps1
@@ -12,7 +12,10 @@ param(
     [Parameter(Mandatory=$false)]
     [string]$startingYear = "2024",
     [Parameter(Mandatory=$false)]
-    [string]$specificVersion = $null
+    [string]$specificVersion = $null,
+    [Parameter(Mandatory=$false)]
+    [string]$nugetApiKey = $null
+
 )
 
 function Get-Version {
@@ -73,7 +76,7 @@ REFERENCE: Updating package: $($_.GetAttribute('Include')) to version $version"
     }
 }
 
-if($hasCounts -or $forcePack -eq 'true')
+if($hasCounts -or $null -ne $nugetApiKey)
 {    
     Write-Output "
 PACKER: Test project references where updated
@@ -113,10 +116,13 @@ PACKER: Restoring...
 
             Write-Host "PACKER: Packaging projects
 "
-            dotnet pack $PWD/SourceCrafter.DependencyInjection/SourceCrafter.DependencyInjection.csproj -c Release -v n -p:PackageVersion=$version
-            dotnet pack $PWD/SourceCrafter.DependencyInjection.Metadata/SourceCrafter.DependencyInjection.Metadata.csproj -c Release -v n -p:PackageVersion=$version
-            dotnet pack $PWD/SourceCrafter.DependencyInjection.MsConfiguration/SourceCrafter.DependencyInjection.MsConfiguration.csproj -c Release -v n -p:PackageVersion=$version
-            dotnet pack $PWD/SourceCrafter.DependencyInjection.MsConfiguration.Metadata/SourceCrafter.DependencyInjection.MsConfiguration.Metadata.csproj -c Release -v n -p:PackageVersion=$version
+            foreach($packageName in $('','.Metadata','.MsConfiguration','MsConfiguration.Metadata'))
+            {
+                dotnet pack $PWD/SourceCrafter.DependencyInjection$packageName/SourceCrafter.DependencyInjection$packageName.csproj -c Release -v n -p:PackageVersion=$version
+                dotnet nuget push $PWD/publish/SourceCrafter.DependencyInjection$packageName.$version.nupkg --api-key $nugetApiKey --source "https://api.nuget.org/v3/index.json"
+                Add-Content -Path "telegram_packages.txt" -Value "| $packageName | [$version](https://www.nuget.org/packages/$packageName/$version) |"
+                Add-Content -Path "github_packages.txt" -Value "[![$packageName`: $version](https://img.shields.io/nuget/v/$packageName.svg?label=$packageName&style=plastic)](https://www.nuget.org/packages/$packageName/$version)"            
+            }
         }
         catch
         {
